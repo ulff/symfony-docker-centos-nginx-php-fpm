385bb204ab88736e60012d8357ec647486f14f85
ulff <ulff.pl@gmail.com>
2015-04-10 10:02:19 +0200

    removed mysql machine

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,5 @@
 FROM jacekelgda/docker-centos-nginx-php-fmp
 
-ENV RDS_HOSTNAME foo
-ENV RDS_DB_NAME foo
-ENV RDS_PASSWORD foo
-ENV RDS_USERNAME foo
-ENV RDS_PORT foo
-
 ADD docker/images/php/container-files /
 
 RUN yum install git -y
--- docker/images/php/container-files/config/init/10-nginx-cache-dirs.sh
+++ docker/images/php/container-files/config/init/10-nginx-cache-dirs.sh
@@ -12,9 +12,9 @@ mkdir -p /run/user/nginx-cache
 mkdir -p /run/user/nginx-cache-tmp
 chown -R www:www /run/user/nginx-cache*
 
-echo fastcgi_param MYSQL_PORT_3306_TCP_ADDR $MYSQL_PORT_3306_TCP_ADDR';' >> /etc/nginx/fastcgi_params
-echo fastcgi_param RDS_HOSTNAME $RDS_HOSTNAME';' >> /etc/nginx/fastcgi_params
-echo fastcgi_param RDS_DB_NAME $RDS_DB_NAME';' >> /etc/nginx/fastcgi_params
-echo fastcgi_param RDS_PASSWORD $RDS_PASSWORD';' >> /etc/nginx/fastcgi_params
-echo fastcgi_param RDS_USERNAME $RDS_USERNAME';' >> /etc/nginx/fastcgi_params
-echo fastcgi_param RDS_PORT $RDS_PORT';' >> /etc/nginx/fastcgi_params
+#echo fastcgi_param MYSQL_PORT_3306_TCP_ADDR $MYSQL_PORT_3306_TCP_ADDR';' >> /etc/nginx/fastcgi_params
+#echo fastcgi_param RDS_HOSTNAME $RDS_HOSTNAME';' >> /etc/nginx/fastcgi_params
+#echo fastcgi_param RDS_DB_NAME $RDS_DB_NAME';' >> /etc/nginx/fastcgi_params
+#echo fastcgi_param RDS_PASSWORD $RDS_PASSWORD';' >> /etc/nginx/fastcgi_params
+#echo fastcgi_param RDS_USERNAME $RDS_USERNAME';' >> /etc/nginx/fastcgi_params
+#echo fastcgi_param RDS_PORT $RDS_PORT';' >> /etc/nginx/fastcgi_params
